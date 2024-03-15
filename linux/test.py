1e1aefbe337ec5cb3bce74518e685c4681bc4753
nihao_lx <1492110355@qq.com>
2024-03-15 21:27:00 +0800

    可自定义位置

--- linux/test.py
+++ linux/test.py
@@ -1,126 +1,125 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
 import schedule
 import time
-from datetime import datetime
+import requests
+from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+headers = {
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Encoding":"gzip, deflate, br, zstd",
+    "Accept-Language":"zh-CN,zh;q=0.9",
+    "Connection":"keep-alive",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    "Host": "hdu.huitu.zhishulib.com",
+    "Referer":"https://hdu.huitu.zhishulib.com/",
+    "Sec-Ch-Ua":"",
+    "Sec-Ch-Ua-Mobile":"?0",
+    "Sec-Ch-Ua-Platform":"Windows",
+}
 
-def login(username, password):
-    driver = None
-    try:
-        driver = webdriver.Chrome(executable_path="./chromedriver", options=options)
-        driver.get("https://hdu.huitu.zhishulib.com/Content/Index/startUp#!/User/Index/login?forward=%2FStation%2FStation%2Flist")
+def login(username,password):
+    for attempt in range(3):
+        driver=None
+        try:
+            chrome_options = Options()
+            chrome_options.add_argument('--headless')  # 无头模式
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            driver = webdriver.Chrome('./chromedriver', options=chrome_options)
 
-        username_input = WebDriverWait(driver, 60).until(
-            EC.presence_of_element_located((By.XPATH, "//input[@placeholder='学号/工号/账号/手机号']"))
-        )
-        username_input.send_keys(username)
+            wait = WebDriverWait(driver, 10)
 
-        password_input = WebDriverWait(driver, 60).until(
-            EC.presence_of_element_located((By.XPATH, "//input[@placeholder='请输入密码']"))
-        )
-        password_input.send_keys(password)
+            pwd_path_selector = """//*[@id="react-root"]/div/div/div[1]/div[2]/div/div[1]/div[2]/div/div/div/div/div[1]/div[2]/div/div[3]/div/div[2]/input"""
+            button_path_selector = """//*[@id="react-root"]/div/div/div[1]/div[2]/div/div[1]/div[2]/div/div/div/div/div[1]/div[3]"""
 
-        login_button = driver.find_element(By.XPATH, "//div[.//span[text()='登录']]")
-        login_button.click()
 
-        print("登录成功！")
-        return True
-    except Exception as e:
-        print(f"登录过程中出现异常：{e}")
-        if driver is not None:
+            driver.get("https://hdu.huitu.zhishulib.com/")
+            #等待加载
+            wait.until(EC.presence_of_element_located((By.NAME, "login_name")))
+            wait.until(EC.presence_of_element_located((By.XPATH, pwd_path_selector)))
+            wait.until(EC.presence_of_element_located((By.XPATH, button_path_selector)))
+            #寻找元素
+            driver.find_element(By.NAME, 'login_name').clear()
+            driver.find_element(By.NAME, 'login_name').send_keys(username)
+            driver.find_element(By.XPATH, pwd_path_selector).clear()
+            driver.find_element(By.XPATH, pwd_path_selector).send_keys(password)
+            driver.find_element(By.XPATH, button_path_selector).click()
+            time.sleep(5)
+
+            cookie_list = driver.get_cookies()
+            cookie = ";".join([f"{item['name']}={item['value']}" for item in cookie_list])
+
+            print(f"用户{username}登录成功！")
+            return {'Cookie': cookie}
+        except Exception as e:
+            print(f"用户{username}登录失败，尝试次数 {attempt + 1}: {e}")
+        finally:
             driver.quit()
-        return False
 
-def select_date():
-    driver = webdriver.Chrome(executable_path="./chromedriver", options=options)
-    driver.get("https://hdu.huitu.zhishulib.com/content/index/startUp/openid/okOhQt_N4h7yYsfY0gWCa4EvBGXA/#!/Seat/Index/searchSeats?space_category%5Bcategory_id%5D=591&space_category%5Bcontent_id%5D=3")
+    return None
 
-    select_elements = WebDriverWait(driver, 60).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, "lrnw-picker"))
-    )
+def select(cookie,seat):
+    for attempt in range(3):
+        try:
+            uid = None
+            try:    #获取uid
+                response=requests.get("https://hdu.huitu.zhishulib.com/Seat/Index/searchSeats?LAB_JSON=1",headers=headers,cookies=cookie)
+                uid=response.json()['DATA']["uid"]
+            except :
+                print("uid参数获取失败！")
+            now = datetime.now()
+            start_time_of_the_day = datetime(now.year, now.month, now.day)
+            target_start_time = start_time_of_the_day + timedelta(days=1, hours=-1) #-1+8=7(7点) day=1,表明是明天
+            duration = 3600
+            begintime = int((target_start_time - datetime(1970, 1, 1)).total_seconds())
 
-    select = Select(select_elements[0])
-    max_value_option = max(select.options, key=lambda option: int(option.get_attribute('value')))
-    select.select_by_value(max_value_option.get_attribute('value'))
-    print(f"使用日期: {max_value_option.text}")
+            data = {
+                "beginTime": begintime,
+                "duration": duration,
+                "seats[0]": seat,
+                "seatBookers[0]": uid,
+                "is_recommend":1,
+            }
 
-    select = Select(select_elements[1])
-    select.select_by_value("2")
-    selected_option = select.first_selected_option
-    print(f"开始时间: {selected_option.text}")
+            response = requests.post("https://hdu.huitu.zhishulib.com/Seat/Index/bookSeats?LAB_JSON=1", data=data, headers=headers,cookies=cookie)
+            #查看预约情况
+            msg=response.json()['MESSAGE']
+            print(f"当前账号{msg}")
+            return True
 
-    select = Select(select_elements[2])
-    max_value_option = max(select.options, key=lambda option: int(option.get_attribute('value')))
-    select.select_by_value(max_value_option.get_attribute('value'))
-    print(f"使用时长: {max_value_option.text}小时")
+        except Exception as e:
+            print(f"预约失败,尝试次数 {attempt + 1}: {e}")
 
-    select = Select(select_elements[3])
-    select.select_by_value("0")
-    selected_option = select.first_selected_option
-    print(f"使用人数: {selected_option.text}人")
+    return None
 
 
-    start_button = WebDriverWait(driver, 60).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[span[contains(text(), '开始选座')]]"))
-    )
-    start_button.click()
-    print("开始选座")
-    print("当前默认你今日使用的座位。")
-
-    confirm_button = WebDriverWait(driver, 60).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'lrnw-touchable') and .//span[contains(text(), '确认预约')]]"))
-    )
-    confirm_button.click()
-    print("选座成功！座位信息如下:")
-
-    seat_element = WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '座位')]/following-sibling::span"))
-    )
-    print(f"座位：{seat_element.text}")
-
-    room_element = WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '房间')]/following-sibling::span"))
-    )
-    print(f"房间：{room_element.text}")
-
-    start_time_element = WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '开始时间')]/following-sibling::span"))
-    )
-    print(f"开始时间：{start_time_element.text}")
-
-    end_time_element = WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '结束时间')]/following-sibling::span"))
-    )
-    print(f"结束时间：{end_time_element.text}")
-
-    driver.quit()
-
-def login_and_select_date(username, password):
-    if login(username, password):
-        select_date()
+def comfirm(username, password,seat):
+    cookie = login(username, password)
+    if cookie:
+        msg=select(cookie,seat)
+        if not msg:
+            print("登录成功，选座失败。")
     else:
-        print("登录失败，无法进行选座")
+        print("登录失败!!!!!!!")
+
 
 def job():
     print("开始选座:", datetime.now())
-    login_and_select_date("21071411", "RJH21071411")
+    #comfirm("dasfsd","dsadv","das") #输入账号，密码，座位
+    comfirm("dasewfsd","dwfdgfsd","sdsaz") #输入账号，密码，座位
 
 def run_scheduled_job():
-    schedule.every().day.at("20:00").do(job)
+    print("开始运行。")
+    schedule.every().day.at("20:01").do(job)
 
     while True:
         schedule.run_pending()
-        time.sleep(30)
+        time.sleep(5)
 
 if __name__ == "__main__":
     run_scheduled_job()
-
-
